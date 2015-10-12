dbff7bdd3eeb630ea23525e455fd4943839f233c
ziggy <ziggy@elephant-bird.net>
2015-10-12 18:52:37 -0400

    select row & grab id

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
-  $('#top500-table').DataTable( {
+  var table = $('#top500-table').DataTable( {
     "ajax": "data/top500.txt",
     "columns": [
       { "data": "a" },
       { "data": "zip" },
-      { "data": "num" }
-    ]
+      { "data": "num" },
+      //hide the 'id' column; id is used later
+      {
+        "data": "id",
+        "visible": false,
+       "searchable": false
+      }
+      
+    ],
+    "order": [[2, "desc"]],
+    // enable select and only allow one selection at a time
+    "select": {
+      "style": 'single'
+    }
+    
   });
+
+  // listen for select event
+  table.on( 'select', function ( e, dt, type, index ) {
+    // this grabs selected row's id
+    table.row(index[0]).data().id
+          
+  });
+  
 });
+
