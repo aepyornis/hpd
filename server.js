6f5d637f91404e5e535b20781c67a4975b16c087
ziggy <ziggy@elephant-bird.net>
2015-10-12 19:26:48 -0400

    get corporate names at /id

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var restify = require('restify');
 var fs = require('fs');
-//var pg = require('pg');
+var pg = require('pg');
+
+var conString = "postgres://mrbuttons@localhost/hpd";
+
+
 
 var server = restify.createServer({
   name: 'Inside-HPD-Data'
@@ -24,17 +28,47 @@ server.get('/top500', function(req, res, next){
   return next();
 });
 
+
+server.get('/id/:id', function(req, res, next){
+  get_corporate_names(req.params.id, function(result){
+    res.send(result);
+    next();
+  });
+});
+
 //serves static files from html folder
 server.get(/.*/, restify.serveStatic({
   'directory': __dirname + '/html',
   'default': 'index.html'
-}))
+}));
+
 
 
 function retrive_corporate_owners_json(fileName, callback) {
   fs.readFile(fileName, 'utf8', function(err, data){
     if (err) throw Error;
     callback(data);
+  });
+}
+
+function get_corporate_names(id, callback) {
+  var query = "SELECT corporationnames FROM corporate_owners WHERE id=$1";
+  var a = [];
+  a.push(Number(id));
+  pg.connect(conString, function(err, client, done){
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(query, a, function(err, result){
+      if (err){
+        console.error('query error', err);
+        callback(null);
+      } else {
+        //callback with results
+        callback(result.rows[0]);
+      }
+      done();
+    });
   });
 }
 
