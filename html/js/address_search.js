d4900b52a528b0f0a768b40475c7af96e2ecde08
aepyornis <ziggy@elephant-bird.net>
2015-11-06 19:22:14 +0000

    fixed bug where failed research  wont reset

--- html/js/address_search.js
+++ html/js/address_search.js
@@ -17,13 +17,23 @@ function address_popup() {
   $('#address-search-popup').modal();
 
   function update_address_search_modal_html(data) {
+    // hide error, display info
+    $('#address-search-popup .modal-body .info').css('display', 'block');
+    $('#address-search-popup .modal-error').css('display', 'none');
+    // update spans with result from AJAX
     $('.corporationname-result').text(data.corporationname);
     $('#business-address-result').text(data.businesshousenumber + ' ' + data.businessstreetname);
     $('#number-buildings-registered-result').text(data.buildingcount);
+    
   }
 }
 
 function deal_with_bad_address_not_in_db(address) {
   var html = '<h5><strong>' + address + '</strong> was not found in the database. Check your spelling or try again with a new address</h5>'
-  $('#address-search-popup .modal-body').html(html);
+  $('#address-search-popup .modal-error').html(html);
+  // show error, hide info
+  $('#address-search-popup .modal-error').css('display', 'block');
+  $('#address-search-popup .modal-body .info').css('display', 'none');
+  
+  
 }
