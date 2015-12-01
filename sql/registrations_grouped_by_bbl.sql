89aead7877cbcf9d6c7c85ecf19bd157dcfc677b
aepyornis <ziggy@elephant-bird.net>
2015-12-01 19:48:31 +0000

    reg is now a table instead of a view for much improved performance

--- sql/registrations_grouped_by_bbl.sql
+++ sql/registrations_grouped_by_bbl.sql
@@ -1,3 +1,6 @@
+
+-- we will use a table instead of a view for better performance
+/*
 CREATE OR REPLACE VIEW
        registrations_grouped_by_bbl
 as SELECT
@@ -13,3 +16,20 @@ as SELECT
    registrations
  GROUP BY bbl, lat, lng, registrationid;
  
+*/
+
+create table registrations_grouped_by_bbl
+as SELECT
+   first(housenumber) as housenumber,
+   first(streetname) as streetname,
+   first(zip) as zip,
+   first( boro) as boro,
+   lat,
+   lng,
+   registrationid,
+   bbl
+FROM
+        registrations
+GROUP BY bbl, lat, lng, registrationid;
+
+create index on registrations_grouped_by_bbl (registrationid);
