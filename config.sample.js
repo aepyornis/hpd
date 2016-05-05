4113fd7e06801ac8ee775653c74bf90e7c858fe2
ziggy <ziggy@elephant-bird.net>
2016-05-05 12:10:11 -0400

    Update setup & website text

--- config.sample.js
+++ config.sample.js
@@ -7,6 +7,7 @@ config.pg.user = 'your-pg-user-name';
 config.pg.password = 'your-pg-password';
 config.pg.database = 'hpd';
 
+config.data.bbls = 'path/to/bbl_lat_lng.txt';
 config.data.registrations = 'path/to/registrations.txt';
 config.data.contacts = 'path/to/contacts.txt';
 
--- setup.sh
+++ setup.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+# create the database if needed
+# createdb hpd
+
 HPD_DATABASE=$(node -e "var c = require('./config.js'); console.log(c.pg.database)")
 HPD_REGISTRATIONS_FILE=$(node -e "var c = require('./config.js'); console.log(c.data.registrations)")
 HPD_CONTACTS_FILE=$(node -e "var c = require('./config.js'); console.log(c.data.contacts)")
+BBL_LAT_LNG=$(node -e "var c = require('./config.js'); console.log(c.data.bbls)")
 
 mkdir -p tmp
+
 printf "Cleaning the data\n"
 cat ${HPD_REGISTRATIONS_FILE} | python data_cleanup.py 16 1> tmp/registrations.txt 2> tmp/registrations_errors.txt
 cat ${HPD_CONTACTS_FILE} | python data_cleanup.py 15 1> tmp/contacts.txt 2> tmp/contacts_errors.txt
@@ -17,24 +22,13 @@ printf "Removed "$(cat tmp/contacts_errors.txt | wc -l)" bad lines from the cont
 HPD_REGISTRATIONS_FILE=$(pwd)/tmp/registrations.txt
 HPD_CONTACTS_FILE=$(pwd)/tmp/contacts.txt
 
-# create the database if needed
-# createdb hpd
-
 printf 'create table and COPY data\n'
 psql -d ${HPD_DATABASE} -f 'sql/schema.sql'
-
-# if [ ! -f registrations.zip ]; then
-# 	printf 'Downloading data'
-# 	wget -O registrations.zip http://www1.nyc.gov/assets/hpd/downloads/misc/Registrations20151201.zip
-# fi
-# if [ ! -f Registration20151130.txt ]; then
-# 	unzip registrations.zip
-#fi
 
 printf 'Inserting data\n'
 psql -d ${HPD_DATABASE} -c "COPY hpd.registrations FROM '"${HPD_REGISTRATIONS_FILE}"' (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
 psql -d ${HPD_DATABASE} -c "COPY hpd.contacts FROM '"${HPD_CONTACTS_FILE}"' (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
-psql -d ${HPD_DATABASE} -c "COPY hpd.bbl_lookup FROM '$(pwd)/bbl_lat_lng.txt' (FORMAT CSV,  HEADER TRUE);"
+psql -d ${HPD_DATABASE} -c "COPY hpd.bbl_lookup FROM '"${BBL_LAT_LNG}"' (FORMAT CSV,  HEADER TRUE);"
 
 printf 'cleanup contact addresses\n'
 psql -d ${HPD_DATABASE} -f 'sql/address_cleanup.sql'
@@ -72,3 +66,12 @@ npm install pg restify async lodash
 printf 'Creating top500.txt file\n'
 mkdir -p html/data
 node get_corporate_owners_json.js 
+
+
+# if [ ! -f registrations.zip ]; then
+# 	printf 'Downloading data'
+# 	wget -O registrations.zip http://www1.nyc.gov/assets/hpd/downloads/misc/Registrations20151201.zip
+# fi
+# if [ ! -f Registration20151130.txt ]; then
+# 	unzip registrations.zip
+#fi
