e28d6a9aa2decf032cd2582a84b00102dc5b3106
ziggy <ziggy@elephant-bird.net>
2017-06-02 13:58:08 -0400

    use schema public instead of hpd

--- sql/index.sql
+++ sql/index.sql
@@ -1,9 +1,9 @@
-create index on hpd.corporate_owners(numberofcontacts desc);
+create index on hpd_corporate_owners(numberofcontacts desc);
 
-create index on hpd.contacts(registrationcontacttype);
-create index on hpd.contacts(registrationid);
-create index on hpd.contacts(registrationid) WHERE  registrationcontacttype = 'CorporateOwner';
+create index on hpd_contacts(registrationcontacttype);
+create index on hpd_contacts(registrationid);
+create index on hpd_contacts(registrationid) WHERE  registrationcontacttype = 'CorporateOwner';
 
-create index on hpd.registrations(registrationid);
-create index on hpd.registrations(lat);
-create index on hpd.registrations(registrationid) where lat IS NOT NULL;
+create index on hpd_registrations(registrationid);
+create index on hpd_registrations(lat);
+create index on hpd_registrations(registrationid) where lat IS NOT NULL;
--- sql/registrations_clean_up.sql
+++ sql/registrations_clean_up.sql
@@ -1,4 +1,4 @@
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' AVE$|-AVE$| -AVE$', ' AVENUE') WHERE streetname ~ '.*(AVE$|-AVE$| -AVE$)';
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' AVE$|-AVE$| -AVE$', ' AVENUE') WHERE streetname ~ '.*(AVE$|-AVE$| -AVE$)';
 
 
 -- unsure what to do about the conflict with streets?
@@ -6,30 +6,30 @@ UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' AVE$|-AV
 
 -- remove periods
 
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '\.', '', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '\.', '', 'g');
 
 -- remove TH ST RD ND
 
-UPDATE hpd.registrations SET streetname = array_to_string(regexp_matches(streetname, '(.*)(\d+)(?:TH|RD|ND|ST)( .+)'), '') WHERE streetname ~ '.*(\d+)(?:TH|RD|ND|ST)( .+).*';
+UPDATE hpd_registrations SET streetname = array_to_string(regexp_matches(streetname, '(.*)(\d+)(?:TH|RD|ND|ST)( .+)'), '') WHERE streetname ~ '.*(\d+)(?:TH|RD|ND|ST)( .+).*';
 
 -- LANE, STREET, ROAD, PARKWAY, BOULEVARD, PLACE, BEACH
 
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' LA$', ' LANE', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' LN$', ' LANE', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' PL$', ' PLACE', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' ST$| STR$', ' STREET', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' RD$', ' ROAD', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' PKWY$', 'PARKWAY', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' PKWY ', ' PARKWAY ', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' BLVD$', ' BOULEVARD', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, ' BLVD ', ' BOULEVARD ', 'g');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '^BCH ', 'BEACH ', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' LA$', ' LANE', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' LN$', ' LANE', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' PL$', ' PLACE', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' ST$| STR$', ' STREET', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' RD$', ' ROAD', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' PKWY$', 'PARKWAY', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' PKWY ', ' PARKWAY ', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' BLVD$', ' BOULEVARD', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, ' BLVD ', ' BOULEVARD ', 'g');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '^BCH ', 'BEACH ', 'g');
 
 -- DIRECTIONS
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '^E ', 'EAST ');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '^W ', 'WEST ');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '^N ', 'NORTH ');
-UPDATE hpd.registrations SET streetname = regexp_replace( streetname, '^S ', 'SOUTH '); 
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '^E ', 'EAST ');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '^W ', 'WEST ');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '^N ', 'NORTH ');
+UPDATE hpd_registrations SET streetname = regexp_replace( streetname, '^S ', 'SOUTH '); 
 
 --UPDATE registrations SET BusinessApartment = regexp_replace( BusinessApartment, '_|\.', '', 'g');
 
